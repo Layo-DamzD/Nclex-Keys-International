c52f3119085dddd60667db21e4edbd2476352e65
Layo-DamzD <oluwalayomi2211@gmail.com>
2026-02-28 06:29:08 +0100

    Fix categories import casing for Vercel build

--- client/src/components/TestCustomization.jsx
+++ client/src/components/TestCustomization.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { CATEGORIES } from '../constants/categories';
+import { CATEGORIES } from '../constants/Categories';
 
 const TestCustomization = () => {
   const [selectedSubcategoryPairs, setSelectedSubcategoryPairs] = useState([]);
