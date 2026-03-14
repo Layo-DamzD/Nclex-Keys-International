779912ef5757b0ad3016e7f36b0f84747479238f
Layo-DamzD <oluwalayomi2211@gmail.com>
2026-03-14 18:00:03 +0100

    Use full country dropdown on student signup

--- client/src/pages/StudentSignup.jsx
+++ client/src/pages/StudentSignup.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { COUNTRIES } from '../constants/Countries';
 
 const DEVICE_STORAGE_KEY = 'nclexkeys:student-device-id';
 const getOrCreateDeviceId = () => {
@@ -255,12 +256,16 @@ const StudentSignup = () => {
 
               <div className="mb-3">
                 <label className="form-label fw-bold">Country</label>
-                <input
-                  type="text"
+                <select
                   className={`form-control ${errors.country ? 'is-invalid' : ''}`}
                   autoComplete="country-name"
                   {...register('country', { required: 'Country is required' })}
-                />
+                >
+                  <option value="">Select country</option>
+                  {COUNTRIES.map((country) => (
+                    <option key={country} value={country}>{country}</option>
+                  ))}
+                </select>
                 {errors.country && <div className="invalid-feedback">{errors.country.message}</div>}
               </div>
 
